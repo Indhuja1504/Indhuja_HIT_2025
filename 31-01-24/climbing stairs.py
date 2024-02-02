90f547b1b129da66f3be823740044b959b4509e2
Indhuja1504 <158060082+Indhuja1504@users.noreply.github.com>
2024-02-02 21:09:07 +0530

    climbing stairs.py

---
+++ 31-01-24/climbing stairs.py
@@ -0,0 +1,36 @@
+#You are climbing a staircase. It takes n steps to reach the top.
+
+#Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+#Example 1:
+
+#Input: n = 2
+#Output: 2
+#Explanation: There are two ways to climb to the top.
+#1. 1 step + 1 step
+#2. 2 steps
+
+
+  class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        # Initialize an array to store the number of ways to reach each step
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+
+        # Use dynamic programming to calculate the number of ways for each step
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
